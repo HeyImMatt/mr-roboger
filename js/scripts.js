81475a2b6348e49f77229f433fc31d4f6cd638b1
heyimmatt <mrmcfarland@gmail.com>
2020-06-12 10:13:34 -0700

    Add logic for handling spec cases for num input

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,44 @@
-function robogerize(num, name) {
-  displayResults(num, name)
+function robogerize(num, name, order) {
+  let outputArr = [];
+  if (num > 0) {
+    if (order === 'ascending') {
+      for (let i = 0; i <= num; i++) {
+        outputArr.push(evaluateNumber(i.toString(), name))
+      }
+    } else {
+      for (let i = num; i >= 0; i--) {
+        outputArr.push(evaluateNumber(i.toString(), name))
+      }
+    }
+  }
+  displayResults(outputArr)
 }
 
-function displayResults(nums, name) {
+function evaluateNumber(number, name) {
+  if (number.includes('3')) {
+    return `Won\'t you be my neighbor, ${name}?`
+  } else if (number.includes('2')) {
+    return 'Boop!'
+  } else if (number.includes('1')) {
+    return 'Beep!'
+  } else return number
+}
+
+function displayResults(outputArr) {
   const outputDiv = document.getElementById('output');
-  outputDiv.append(`${nums}`);
+  outputDiv.append(`${outputArr}`);
 }
 
 document.querySelectorAll('button').forEach((button) => {
-  button.addEventListener('click', (event => {
+  button.addEventListener('click', (event) => {
     event.preventDefault();
-    const num = document.getElementById('num-input').value;
+    const num = parseInt(document.getElementById('num-input').value);
     const name = document.getElementById('name').value;
+    const order = event.target.id;
+
     if (!num || !name) {
-      alert('Please fill out the fields!')
-    } else robogerize(num, name)
-  }));
+      alert('Please fill out the fields!');
+    } else robogerize(num, name, order);
+
+  });
 });
